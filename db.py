4972b776b382ae4f561dc82ba55c72d3efbddac3
Anish Patil <anishpatil304@gmail.com>
2025-10-12 18:40:58 +0530

    env and config

--- db.py
+++ db.py
@@ -1,13 +1,21 @@
+import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-URL_DATABASE = "postgresql://postgres:admin%40123@localhost:5432/teamflow"
+# Load environment variables from .env
+load_dotenv()
 
+# Read DATABASE_URL from environment
+URL_DATABASE = os.getenv("DATABASE_URL")
+
+if not URL_DATABASE:
+    raise ValueError("DATABASE_URL not found. Please set it in the .env file.")
+
+# Initialize SQLAlchemy
 engine = create_engine(URL_DATABASE, echo=True)
-
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 Base = declarative_base()
 
 def get_db():
